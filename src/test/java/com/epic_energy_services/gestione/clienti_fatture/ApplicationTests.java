b603b1859dc164afbdfc3491b6a3373b529f8ae2
nbadea00 <nbadea61@gmail.com>
2023-03-20 09:02:33 +0100

    first-commit

---
+++ src/test/java/com/epic_energy_services/gestione/clienti_fatture/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epic_energy_services.gestione.clienti_fatture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
