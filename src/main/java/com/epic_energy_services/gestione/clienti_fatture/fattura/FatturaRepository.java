3207af3304bed208c372a55e8ab5cbb7ed6fc55c
SteVioli <stefano.violi87@gmail.com>
2023-03-22 12:20:45 +0100

    fatture query

--- src/main/java/com/epic_energy_services/gestione/clienti_fatture/fattura/FatturaRepository.java
+++ src/main/java/com/epic_energy_services/gestione/clienti_fatture/fattura/FatturaRepository.java
@@ -1,7 +1,24 @@
 package com.epic_energy_services.gestione.clienti_fatture.fattura;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface FatturaRepository extends CrudRepository<Fattura, Long> {
+import com.epic_energy_services.gestione.clienti_fatture.cliente.Cliente;
 
+public interface FatturaRepository extends CrudRepository<Fattura, Long> {
+				
+	 List<Fattura> findByCliente(Cliente c);
+
+	 List<Fattura> findByStatoFattura(StatoFattura statoFattura);
+
+	 List<Fattura> findByData(Date data);
+
+	 List<Fattura> findByAnno(Integer anno);
+	 
+	 List<Fattura> findByImportoBetween(BigDecimal importoIniziale, BigDecimal importoFinale);
+
+	
 }
--- src/main/java/com/epic_energy_services/gestione/clienti_fatture/fattura_controller/FatturaController.java
+++ src/main/java/com/epic_energy_services/gestione/clienti_fatture/fattura_controller/FatturaController.java
@@ -1,7 +1,10 @@
 package com.epic_energy_services.gestione.clienti_fatture.fattura_controller;
 
+import java.math.BigDecimal;
+import java.sql.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,12 +14,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.epic_energy_services.gestione.clienti_fatture.cliente.Cliente;
 import com.epic_energy_services.gestione.clienti_fatture.cliente.ClienteService;
 import com.epic_energy_services.gestione.clienti_fatture.fattura.Fattura;
 import com.epic_energy_services.gestione.clienti_fatture.fattura.FatturaService;
+import com.epic_energy_services.gestione.clienti_fatture.fattura.StatoFattura;
 
 @RestController
 @RequestMapping("/fatture")
@@ -69,11 +74,44 @@ public class FatturaController {
 		return ResponseEntity.ok("Fattura Cancellata!");
 	}
 	
-	@GetMapping("cliente/{id}")
+	
+	@GetMapping("/filtra/cliente={id}")
 	@PreAuthorize("hasRole('USER')")
-	public ResponseEntity<Cliente> getClienteById(@PathVariable Long id){
-		return ResponseEntity.ok(clienteServ.findById(id));
+	public ResponseEntity<List<Fattura>> trovaFatturaByCliente(@PathVariable Long id){
+		return ResponseEntity.ok(fatturaServ.getFatturaByIdCliente(id));
 	}
+	
+	@GetMapping("/filtra/stato={stato}")
+	@PreAuthorize("hasRole('USER')")
+	public ResponseEntity<List<Fattura>> trovaFatturaByStato(@PathVariable StatoFattura stato){
+		return ResponseEntity.ok(fatturaServ.getFatturaByStatoFattura(stato));
+	}
+	
+	@GetMapping("/filtra/anno={anno}")
+	@PreAuthorize("hasRole('USER')")
+	public ResponseEntity<List<Fattura>> trovaFatturaByAnno(@PathVariable Integer anno){
+		return ResponseEntity.ok(fatturaServ.getFatturaByAnno(anno));
+	}
+	
+	@GetMapping("/filtra/importo1={imp1}/importo2={imp2}")
+	@PreAuthorize("hasRole('USER')")
+	public ResponseEntity<List<Fattura>> trovaFatturaByImportoRange(
+			@PathVariable BigDecimal imp1,@PathVariable BigDecimal imp2){
+		return ResponseEntity.ok(fatturaServ.getFatturaByRangeImport(imp1, imp2));
+		}
+
+//	@GetMapping("/filtra/importo1?&")
+//	@PreAuthorize("hasRole('USER')")
+//	public ResponseEntity<String> trovaFatturaByImportoRange(
+//			@RequestParam BigDecimal imp1,@RequestParam BigDecimal imp2){
+//		return new ResponseEntity<String>("asd", HttpStatus.OK);}
+	
+	@GetMapping("/filtra/data={data}")
+	@PreAuthorize("hasRole('USER')")
+	public ResponseEntity<List<Fattura>> trovaFatturaByData(@PathVariable Date data){
+		return ResponseEntity.ok(fatturaServ.getFatturaByData(data));
+	}
+	
 	
 	@PostMapping("/cliente")
 	@PreAuthorize("hasRole('ADMIN')")
